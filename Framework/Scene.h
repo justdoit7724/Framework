4ff636e764e277b355879fc5513bcf75f41a9f72
ChangJun Kim <justdoit7724@gmail.com>
2019-09-05 16:24:07 -0400

    Revert "debugging code in scene"
    This reverts commit 4e998ee29a0a362af0643d396e43f5e9f86f52a0.

--- Framework/Scene.h
+++ Framework/Scene.h
@@ -11,17 +11,9 @@ class DirectionalLight;
 class PointLight;
 class SpotLight;
 class UICanvas;
-
-//delete
-#include "Resource.h"
 struct SHADER_DIRECTIONAL_LIGHT;
 struct SHADER_POINT_LIGHT;
 struct SHADER_SPOT_LIGHT;
-struct ShaderMaterial;
-struct VS_Property;
-class Hill;
-class Sphere;
-class VPShader;
 
 class Scene
 {
@@ -41,20 +33,5 @@ private:
 	std::vector<Object*> objs;
 
 	DirectionalLight* dLight;
-
-	//delete
-	Hill* hill;
-	Sphere* sphere;
-	Object* temp;
-	VPShader* shader;
-	ShaderMaterial* material;
-	ConstantBuffer<VS_Property>* cb_vs_property;
-	ConstantBuffer<SHADER_DIRECTIONAL_LIGHT>* cb_ps_dLights;
-	ConstantBuffer<SHADER_POINT_LIGHT>* cb_ps_pLights;
-	ConstantBuffer<SHADER_SPOT_LIGHT>* cb_ps_sLights;
-	ConstantBuffer<XMFLOAT3>* cb_ps_eyePos;
-	ConstantBuffer<ShaderMaterial>* cb_ps_material;
-	ID3D11ShaderResourceView* bodySrv;
-	ID3D11SamplerState* samplerState;
 };
 
