27589bbc7169525ab8a2a95930b092a6bd287462
ChangJun Kim <justdoit7724@gmail.com>
2019-10-29 18:47:18 -0400

    [backup] custom

--- Framework/AOVS.hlsl
+++ Framework/AOVS.hlsl
@@ -3,8 +3,8 @@
 struct VS_OUTPUT
 {
     float4 pos : SV_POSITION;
-    float3 vNormal : TEXCOORD0;
-    float vDepth : TEXCOORD1;
+    float vDepth : TEXCOORD0;
+    float3 vNormal : TEXCOORD1;
 };
 
 cbuffer CB_TRANSF_AO : register(b0)
@@ -18,11 +18,11 @@ cbuffer CB_TRANSF_AO : register(b0)
 VS_OUTPUT main(STD_VS_INPUT input)
 {
     VS_OUTPUT output;
-    float4 wPos = mul(worldMat, float4(input.pos, 1));
-    float4 vPos = mul(viewMat, wPos);
-    output.vDepth = vPos.z;
-    output.pos = mul(projMat, vPos);
-    float3 wNormal = mul((float3x3) worldMat, input.normal);
+    float4 worldPos = mul(worldMat, float4(input.pos, 1));
+    float4 viewPos = mul(viewMat, worldPos);
+    output.vDepth = viewPos.z;
+    output.pos = mul(projMat, viewPos);
+    float3 wNormal = mul((float3x3) normalMat, input.normal);
     output.vNormal = mul((float3x3) viewMat, wNormal);
     return output;
 }
