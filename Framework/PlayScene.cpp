8ce667547f64285a3eac97eaff88bea3c1d2b558
jun <justdoit7724@gmail.com>
2020-04-08 19:55:56 +0900

    Clean after merging from branch

--- Framework/PlayScene.cpp
+++ Framework/PlayScene.cpp
@@ -5,20 +5,12 @@
 #include "Game_info.h"
 #include "Light.h"
 #include "Buffer.h"
-#include "Tictactoe.h"
 #include "Keyboard.h"
-#include "TTTAI.h"
-#include "TTTRender.h"
 #include "Debugging.h"
 #include "CustomSTL.h"
 
 PlayScene::PlayScene()
 {
-	render = new TTTRender();
-	rule = new Tictactoe(&render);
-	ai = new TTTAI(&rule, &render);
-	render->SetClickFunc(rule);
-
 	dLight = new DirectionalLight(
 		XMFLOAT3(0.4, 0.4, 0.4),
 		XMFLOAT3(0.7, 0.7, 0.7),
@@ -26,21 +18,9 @@ PlayScene::PlayScene()
 		Normalize(XMFLOAT3(1, -0.5, 1)));
 
 	cbEye = new Buffer(sizeof(XMFLOAT4));
-
-	Debugging::Instance()->Enabled(false);
 }
 
 
 void PlayScene::Update(float elapsed, float spf)
 {
-	render->Update();
-
-	if (Keyboard::GetKey('A') == KeyState_Up)
-	{
-		rule->JumpBackward();
-	}
-	else if (Keyboard::GetKey('D') == KeyState_Up)
-	{
-		rule->JumpForward();
-	}
 }
--- Framework/PlayScene.h
+++ Framework/PlayScene.h
@@ -3,11 +3,6 @@
 
 class DirectionalLight;
 class Buffer;
-class Tictactoe;
-class TTTRender;
-class TTTAI;
-
-#include <Nlohmann-JSON.3.7.3\build\native\include\nlohmann\json.hpp>
 
 class PlayScene : public Scene
 {
@@ -16,11 +11,6 @@ public:
 	void Update(float elapsed, float spf) override;
 
 private:
-
-	
-	Tictactoe* rule;
-	TTTRender* render;
-	TTTAI* ai;
 
 	DirectionalLight* dLight;
 	Buffer* cbEye;
